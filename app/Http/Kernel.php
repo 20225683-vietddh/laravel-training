5d903b9781174b660089caaabb855952bec2fe4c
Din19062004 <haivietb9@gmail.com>
2025-08-01 17:19:57 +0700

    done chap08: auth
    done chap07: middleware
    
    done chap08: auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,10 +46,10 @@ class Kernel extends HttpKernel
     ];
 
     /**
-     * The application's route middleware.
+     * The application's middleware aliases.
      *
-     * These middleware may be assigned to groups or used individually.
-     *
+     * Aliases may be used instead of class names to conveniently assign middleware to routes and groups.
+     * 
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\CheckAdmin::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +21,24 @@ Route::get('/', function () {
 });
 
 Route::get('/users', [UserController::class, 'index'])->name('user.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('user.create');
+Route::get('/users/create', [UserController::class, 'create'])
+    ->name('user.create')
+    ->middleware(['admin']);
 Route::get('/users/{user}', [UserController::class, 'show'])->name('user.show');
 Route::post('/users', [UserController::class, 'store'])->name('user.store');
 Route::get('/users/edit', [UserController::class, 'edit'])->name('user.edit');
 Route::put('/users/{user}', [UserController::class, 'update'])->name('user.update');
 Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('user.destroy');
 Route::resource('tasks', TaskController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
